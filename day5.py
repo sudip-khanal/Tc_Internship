1988ebe72b946b6a30419fe08f153e29e2effd57
sudip-khanal <sudipkhanal0777@gmail.com>
2024-06-09 15:15:57 +0545

    day5 exercise

--- day5.py
+++ day5.py
@@ -1,4 +1,6 @@
 
+# context manager
+
 class FileManager():
 	def __init__(self, filename, mode):
 		self.filename = filename
@@ -16,4 +18,47 @@ class FileManager():
 with FileManager('test1.txt', 'w') as f:
 	f.write('Hello!!!!')
 
-print(f.closed)
+
+class FileManager():
+	def __init__(self, filename, mode):
+		self.filename = filename
+		self.mode = mode
+		self.file = None
+		
+	def __enter__(self):
+		self.file = open(self.filename, self.mode)
+		return self.file
+	
+	def __exit__(self, exc_type, exc_value, exc_traceback):
+		self.file.close()
+
+# loading a file 
+with FileManager('test2.txt', 'r+') as f:
+	f.write('This is test file 2 with read and wright permission')
+
+
+
+
+#threading
+
+import threading
+
+def print_cube(num):
+	print("Cube: {}" .format(num * num * num))
+
+
+def print_square(num):
+	print("Square: {}" .format(num * num))
+
+
+if __name__ =="__main__":
+	t1 = threading.Thread(target=print_square, args=(10,))
+	t2 = threading.Thread(target=print_cube, args=(10,))
+
+	t1.start()
+	t2.start()
+
+	t1.join()
+	t2.join()
+
+	print("Done!")
