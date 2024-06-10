46c99dd34093b17e5d2170d1e60c1747320284f3
sudip-khanal <sudipkhanal0777@gmail.com>
2024-06-10 11:14:46 +0545

    change in exercise.py

--- exercise.py
+++ exercise.py
@@ -133,6 +133,29 @@ print(Counter.count)
 Counter.increment()
 print(Counter.count)
 
+# threading
+import threading
+
+def print_cube(num):
+	print("Cube: {}" .format(num * num * num))
+
+
+def print_square(num):
+	print("Square: {}" .format(num * num))
+
+
+if __name__ =="__main__":
+	t1 = threading.Thread(target=print_square, args=(10,))
+	t2 = threading.Thread(target=print_cube, args=(10,))
+
+	t1.start()
+	t2.start()
+
+	t1.join()
+	t2.join()
+
+	print("Done!")
+
 # Create a class Date with attributes day, month, and year. Add a class 
 # method today that returns a new instance of Date with the current date.
 
